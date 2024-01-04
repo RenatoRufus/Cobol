021bfbcbfeb54bc2d03f022504a4ff61c18344e3
RenatoRufus <renatooffice1@gmail.com>
2024-01-04 16:52:55 -0300

    cobol um

---
+++ hello.cbl
@@ -0,0 +1,13 @@
+************************************************************************
+*Author:
+*Date:
+*Purpose:
+*Tectonics:
+************************************************************************
+IDENTIFICATION DIVISION.
+PROGRAM-ID. HELLO. 
+*>SIMPLE HELLO WORLD PROGRAMM
+PROCEDURE DIVISION.
+    DISPLAY 'HELLO WORLD'.
+    STOP RUN.
+
---
+++ helloworld.cbl
@@ -0,0 +1,8 @@
+identification division.
+author. "Rufus".
+program-id. "hello".
+
+procedure division.
+display "Hello world".
+
+
